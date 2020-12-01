c1ce79454537a5d6b79fbc2eefa37a3409108c40
JesusfGarcia <jesusfgarciavaldez@gmail.com>
2020-12-01 00:57:40 -0700

    cambiando nav por tipo de usuario

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -7,41 +7,79 @@ const MiTienda = React.lazy(() => import("../Views/MiTienda"));
 const Principal = React.lazy(() => import("../Views/Principal"));
 const Profile = React.lazy(() => import("../Views/Profile"));
 
-export const Routes = [
-  {
-    route: "/home/Principal",
-    label: "Inicio",
-    component: Principal,
-    exact: true,
-  },
-  {
-    route: "/home/MiTienda",
-    label: "Mi Tienda",
-    component: MiTienda,
-    exact: true,
-  },
-  {
-    route: "/home/Empresas",
-    label: "Empresas",
-    component: Empresas,
-    exact: true,
-  },
-  {
-    route: "/home/Favoritos",
-    label: "Favoritos",
-    component: Favoritos,
-    exact: true,
-  },
-  {
-    route: "/home/Mercado",
-    label: "Mercado",
-    component: Mercado,
-    exact: true,
-  },
-  {
-    route: "/home/perfil",
-    label: "Mi perfil",
-    component: Profile,
-    exact: true,
-  },
-];
+let Routes = [];
+
+if (localStorage.getItem("rol") === "Visitante") {
+  Routes = [
+    {
+      route: "/home/Principal",
+      label: "Inicio",
+      component: Principal,
+      exact: true,
+    },
+    {
+      route: "/home/Empresas",
+      label: "Empresas",
+      component: Empresas,
+      exact: true,
+    },
+    {
+      route: "/home/Favoritos",
+      label: "Favoritos",
+      component: Favoritos,
+      exact: true,
+    },
+    {
+      route: "/home/Mercado",
+      label: "Mercado",
+      component: Mercado,
+      exact: true,
+    },
+    {
+      route: "/home/perfil",
+      label: "Mi perfil",
+      component: Profile,
+      exact: true,
+    },
+  ];
+} else {
+  Routes = [
+    {
+      route: "/home/Principal",
+      label: "Inicio",
+      component: Principal,
+      exact: true,
+    },
+    {
+      route: "/home/MiTienda",
+      label: "Mi Tienda",
+      component: MiTienda,
+      exact: true,
+    },
+    {
+      route: "/home/Empresas",
+      label: "Empresas",
+      component: Empresas,
+      exact: true,
+    },
+    {
+      route: "/home/Favoritos",
+      label: "Favoritos",
+      component: Favoritos,
+      exact: true,
+    },
+    {
+      route: "/home/Mercado",
+      label: "Mercado",
+      component: Mercado,
+      exact: true,
+    },
+    {
+      route: "/home/perfil",
+      label: "Mi perfil",
+      component: Profile,
+      exact: true,
+    },
+  ];
+}
+export { Routes };
